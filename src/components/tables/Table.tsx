ab6b2738790ba6baf85e780b85dc118b89ed07be
Rahu <rahunandan@gmail.com>
2023-06-02 12:56:46 +0200

    feat: performance

--- src/components/tables/Table.tsx
+++ src/components/tables/Table.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo, useCallback } from "react";
 import classNames from "classnames";
 import "./Table.scss";
 
@@ -10,7 +10,7 @@ interface Column {
 }
 
 interface TableProps {
-  data: any[];
+  data: Record<string, string | number>[];
   columns: Column[];
   radio?: boolean;
   checkbox?: boolean;
@@ -22,18 +22,20 @@ const Table: React.FC<TableProps> = ({ data, columns, radio, checkbox }) => {
   const [sortedColumn, setSortedColumn] = useState<string>("");
   const [sortDirection, setSortDirection] = useState<"asc" | "desc">("asc");
 
-  const handleCheckboxSelect = (
-    event: React.ChangeEvent<HTMLInputElement>,
-    row: any
-  ) => {
-    if (event.target.checked) {
-      setSelectedRows([...(selectedRows as string[]), row.id]);
-    } else {
-      setSelectedRows((selectedRows as string[]).filter((id) => id !== row.id));
-    }
-  };
+  const handleCheckboxSelect = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>, row: any) => {
+      if (event.target.checked) {
+        setSelectedRows((prevRows) => [...prevRows, row.id]);
+      } else {
+        setSelectedRows(
+          (selectedRows as string[]).filter((id) => id !== row.id)
+        );
+      }
+    },
+    [selectedRows]
+  );
 
-  const renderCellContent = (row: any, column: Column) => {
+  const renderCellContent = useCallback((row: any, column: Column) => {
     const { key, render, type } = column;
 
     if (!row[key]) return null;
@@ -42,103 +44,121 @@ const Table: React.FC<TableProps> = ({ data, columns, radio, checkbox }) => {
       return render(row[key], row);
     }
 
-    //check if row.id is in selectedRows
-    // if (selectedRows?.includes(row.id)) {
-    //   return <span className="selected">{row[key]}</span>;
-    // }
-
     return row[key];
-  };
+  }, []);
 
-  const handleHeaderClick = (column: Column) => {
-    if (sortedColumn === column.key) {
-      setSortDirection(sortDirection === "asc" ? "desc" : "asc");
-    } else {
-      setSortedColumn(column.key);
-      setSortDirection("asc");
-    }
-  };
-
-  const sortData = (data: any[]) => {
-    if (sortedColumn) {
-      return data.sort((a, b) => {
-        const valueA = a[sortedColumn];
-        const valueB = b[sortedColumn];
-
-        if (valueA < valueB) {
-          return sortDirection === "asc" ? -1 : 1;
-        }
-        if (valueA > valueB) {
-          return sortDirection === "asc" ? 1 : -1;
-        }
-        return 0;
-      });
-    }
-    return data;
-  };
-
-  const renderHeader = () => (
-    <tr>
-      {radio || checkbox ? <th></th> : null}
-      {columns.map((column) => (
-        <th
-          key={column.key}
-          className={classNames({
-            "sort-asc": sortedColumn === column.key && sortDirection === "asc",
-            "sort-desc":
-              sortedColumn === column.key && sortDirection === "desc",
-          })}
-          onClick={() => handleHeaderClick(column)}
-        >
-          {column.label}
-        </th>
-      ))}
-    </tr>
+  const handleHeaderClick = useCallback(
+    (column: Column) => {
+      if (sortedColumn === column.key) {
+        setSortDirection(sortDirection === "asc" ? "desc" : "asc");
+      } else {
+        setSortedColumn(column.key);
+        setSortDirection("asc");
+      }
+    },
+    [sortedColumn, sortDirection]
   );
 
-  const renderSelectionButon = (row: any) => {
-    if (radio) {
-      return (
-        <td>
-          <input
-            type="radio"
-            id={row.id}
-            className={"selectionBtn"}
-            checked={selectedRows?.includes(row.id)}
-            onChange={() => setSelectedRows([row.id])}
-          />
-        </td>
-      );
-    } else if (checkbox) {
-      return (
-        <td>
-          <input
-            type="checkbox"
-            className={"selectionBtn"}
-            id={row.id}
-            onChange={(event) => handleCheckboxSelect(event, row)}
-          />
-        </td>
-      );
-    } else {
-      return null;
-    }
-  };
+  const sortData = useMemo(
+    () => (data: any[]) => {
+      if (sortedColumn) {
+        return data.sort((a, b) => {
+          const valueA = a[sortedColumn];
+          const valueB = b[sortedColumn];
 
-  const renderRows = () =>
-    sortData(data).map((row) => (
-      <tr
-        key={row.id}
-        className={classNames({
-          "selected-row": selectedRows?.includes(row.id),
-        })}
-      >
-        {renderSelectionButon(row)}
+          if (valueA < valueB) {
+            return sortDirection === "asc" ? -1 : 1;
+          }
+          if (valueA > valueB) {
+            return sortDirection === "asc" ? 1 : -1;
+          }
+          return 0;
+        });
+      }
+      return data;
+    },
+    [data, sortedColumn, sortDirection]
+  );
+
+  const renderHeader = useCallback(
+    () => (
+      <tr>
+        {radio || checkbox ? <th></th> : null}
         {columns.map((column) => (
-          <td key={column.key}>{renderCellContent(row, column)}</td>
+          <th
+            key={column.key}
+            className={classNames({
+              "sort-asc":
+                sortedColumn === column.key && sortDirection === "asc",
+              "sort-desc":
+                sortedColumn === column.key && sortDirection === "desc",
+            })}
+            onClick={() => handleHeaderClick(column)}
+          >
+            {column.label}
+          </th>
         ))}
       </tr>
-    ));
+    ),
+    [columns, sortedColumn, sortDirection, radio, checkbox, handleHeaderClick]
+  );
+
+  const renderSelectionButon = useCallback(
+    (row: any) => {
+      if (radio) {
+        return (
+          <td>
+            <input
+              type="radio"
+              id={row.id}
+              className={"selectionBtn"}
+              checked={selectedRows?.includes(row.id)}
+              onChange={() => setSelectedRows([row.id])}
+            />
+          </td>
+        );
+      } else if (checkbox) {
+        return (
+          <td>
+            <input
+              type="checkbox"
+              className={"selectionBtn"}
+              id={row.id}
+              onChange={(event) => handleCheckboxSelect(event, row)}
+            />
+          </td>
+        );
+      } else {
+        return null;
+      }
+    },
+    [radio, checkbox, selectedRows, handleCheckboxSelect, setSelectedRows]
+  );
+
+  const renderRows = useCallback(
+    () =>
+      sortData(data).map((row) => (
+        <tr
+          key={row.id}
+          className={classNames({
+            "selected-row": selectedRows?.includes(row.id),
+          })}
+        >
+          {renderSelectionButon(row)}
+          {columns.map((column) => (
+            <td key={column.key}>{renderCellContent(row, column)}</td>
+          ))}
+        </tr>
+      )),
+    [
+      columns,
+      selectedRows,
+      sortData,
+      renderSelectionButon,
+      renderCellContent,
+      selectedRows.length,
+    ]
+  );
 
   return (
     <>
